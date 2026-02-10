13fee8d3041d2c848f97d4e926294fea278b1ff0
valkyriegerald828-jpg <valkyriegerald828@gmail.com>
2026-02-10 13:50:10 +0700

    hari ini

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="flex justify-between items-center px-10 py-5 relative z-20">
+    <div class="flex items-center gap-2.5">
+        <!-- LOGO -->
+        <img src="{{ asset('images/neperlogo.png') }}"
+             alt="Logo"
+             class="w-8 h-8 md:w-9 md:h-9 object-contain
+                    drop-shadow-[0_0_4px_rgba(212,175,55,0.3)]">
+
+        <!-- TITLE -->
+        <h1 class="text-xl md:text-2xl font-bold tracking-wider text-[#d4af37] leading-none">
+            <span class="text-white">NEPER</span>FUTSAL
+        </h1>
+    </div>
+
+    <div class="flex items-center gap-3">
+    <!-- LOGO -->
+    <img src="{{ asset('images/strugglefm.png') }}"
+     alt="Logo"
+     class="h-8 md:h-9 w-auto
+            max-w-[160px] md:max-w-[200px]
+            object-contain">
+    </div>
+</nav>
